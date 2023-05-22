d32ec3c9514d82cd15b70eb5a2e1254c078e941b
Riley Leff <rileyleff@gmail.com>
2023-05-22 07:27:43 +0000

    working

---
+++ exercises/1_hi.py
@@ -0,0 +1,15 @@
+# howdy!
+
+# this is a comment. the python interpreter ignores any text that follows a #.
+
+# let's get our interpreter working.
+
+# try running this line alone in an interactive python session (REPL) with the shortcut Shift + Enter
+print('hello world')
+
+# now try passing this whole script to the interpreter by running the command:
+#       python exercises/1_hi.py
+print('wassup')
+
+
+
